30f8326e8d311412282a123d73947dfedeb21d05
Ja0n0aJ <jana.janzen1@web.de>
2024-07-01 10:25:42 +0000

    02/02 Deklaration und Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -5,3 +5,19 @@
  * 
  * 
  */
+
+
+
+
+
+
+
+
+// Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung / assignment
+console.log(firstName); // Ausgabe
+let familyName = "Mütze";
+console.log("Hi, ich bin " + firstName +" " + familyName + "!"); //--> HI,ich bin Max Mütze!
