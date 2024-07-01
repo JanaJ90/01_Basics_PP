a85c1774b94960eb56a2a29d9c43c3cd6f882324
Ja0n0aJ <jana.janzen1@web.de>
2024-07-01 10:51:31 +0000

    02/02 Datentypen

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -16,8 +16,32 @@
 // Deklaration + Wertzuweisung I */
 
 let firstName; // Deklaration (Definition)
-console.log(firstName); // Ausgabe
+//console.log(firstName); // Ausgabe
 firstName = "Max"; // Wertzuweisung / assignment
-console.log(firstName); // Ausgabe
+//console.log(firstName); // Ausgabe
 let familyName = "Mütze";
-console.log("Hi, ich bin " + firstName +" " + familyName + "!"); //--> HI,ich bin Max Mütze!
+//console.log("Hi, ich bin " + firstName +" " + familyName + "!"); //--> HI,ich bin Max Mütze!
+
+// Wertzuweisung II + Datentypen
+
+let test;
+//test = "HI";
+//test = 2;
+console.log("Datentyp:" + typeof test);
+console.log("Inhalt:" +test);
+console.log("----------");
+
+test = "hi";
+console.log("Datentyp:" + typeof test);
+console.log("Inhalt:" +test);
+console.log("----------");
+
+test = 2;
+console.log("Datentyp:" + typeof test);
+console.log("Inhalt:" +test);
+console.log("----------");
+
+test = true;
+console.log("Datentyp:" + typeof test);
+console.log("Inhalt:" +test);
+console.log("----------");
