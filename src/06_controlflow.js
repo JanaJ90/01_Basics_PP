8f163cd04f48d4f356d218dda1cbc41f2a9e435d
Ja0n0aJ <jana.janzen1@web.de>
2024-07-03 10:42:17 +0000

    fallunterscheidung Switsh

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -12,11 +12,11 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+//console.log("ageJohn: " + ageJohn);
+//console.log("ageMark: " + ageMark);
+//console.log("isJohnOlder: " + isJohnOlder);
+//console.log("isJohnEqual: " + isJohnEqual);
+//console.log("------------------");
 
 /************ IF  ************/
 // TINA --> There is no alternative!
@@ -56,15 +56,67 @@ console.log("------------------");
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if (isJohnOlder) {console.log("John ist älter."); // true
+//if (isJohnOlder) {console.log("John ist älter."); // true
     
-} 
-else if (isJohnEqual)
+
+//else if (isJohnEqual)
  {
-    console.log("John ist gleich alt."); //alt true
+    //console.log("John ist gleich alt."); //alt true
     
 }
-else 
+//else 
 {
-    console.log("John ist junger."); // false
+    //console.log("John ist junger."); // false
+
+
 }
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+ //const firstName = "Jane";
+ //let job;
+
+//job = "driver";  // .. fährt TAXI! / UBER
+//job = "diver"; // .. taucht im Rhein! 
+//job = "artist"; // .. malt ein Bild!
+// job = "pilot"; // .. macht etwas anderes! --> default
+ //job = "teacher"; // .. unterrichtet!
+ //job = "instructor"; // .. unterrichtet!
+
+//switch (job)
+ {
+  //  case "driver":
+    //    console.log(firstName + ".. fährt TAXI");
+      //   break;
+    //case "diver":
+      //  console.log(firstName + ".. taucht in Reihn");
+        //break;
+        //case "artist":
+          //  console.log(firstName + ".. malt ein Bild");
+        //break;
+        //case "teacher":
+        //case "instructor":
+          //  console.log(firstName + ".. unterichtet");
+            //break;
+        
+
+    //default: // wichtig !!!
+      //  console.log(firstName + ".. macht etwas anderes");
+        //break;
+}
+
+/********  Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const value = 10;
+
+switch (cond) {
+    case (value == 1):
+        console.log("Value hat den Wert 1.");
+        break;
+    case (value == 2):
+        console.log("Value hat den Wert 2.");
+        break;
+    default:
+        console.log("Value hat einen anderen Wert.");
+        break;
+    }
