c8eb71d587fe19dd380a169b408bbc02bf34d8c4
Ja0n0aJ <jana.janzen1@web.de>
2024-07-04 09:05:46 +0000

    08/ 2b Funktion

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -13,11 +13,23 @@
 /****Funktionen 02a *****/
 // 2a. Parametrisirung + Datenübergabe von INNEN
 
-//Call
-ausgabeNamen();
-//Funktion
-function ausgabeNamen() {
-       // interne Variable
-    let firstName = "Jana";
-    console.log("Hallo, " + firstName + "!");
+// //Call
+// ausgabeNamen();
+// //Funktion
+// function ausgabeNamen() {
+//        // interne Variable
+//     let firstName = "Jana";
+//     console.log("Hallo, " + firstName + "!");
+// }
+
+// 2b. Parametrisirung + Datenübergabe von AUSSEN
+
+ausgabeNamen2("Jana");
+
+function ausgabeNamen2(firstName) {
+     console.log("Hallo, " + firstName + "!");
+    }
+
+    
+
     
