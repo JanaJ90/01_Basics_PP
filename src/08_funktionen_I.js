9bf570dac489c4cface78b6a43243c8bfe6ceb00
Ja0n0aJ <jana.janzen1@web.de>
2024-07-04 10:04:49 +0000

    08/ 3 funktion

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -24,12 +24,41 @@
 
 // 2b. Parametrisirung + Datenübergabe von AUSSEN
 
-ausgabeNamen2("Jana");
+// ausgabeNamen2("Jana");
 
-function ausgabeNamen2(firstName) {
-     console.log("Hallo, " + firstName + "!");
-    }
+// function ausgabeNamen2(firstName) {
+//      console.log("Hallo, " + firstName + "!");
+//     }
 
-    
+// 2c. Meherere Parameter / Argumente
+
+// ausgabeNamenParams("Jana", "Janzen");
+
+// function ausgabeNamenParams(firstName, familyName) {
+//     console.log("Hallo, " + firstName + " " + familyName + "!");
+// }
+
+// const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamenParams(prompt("Vorname?:"),prompt("Name?:"));
+
+function ausgabeNamenParams(firstName, familyName) {
+         console.log("Hallo, " + firstName + " " + familyName + "!");
+}
+
+//*****Funktion 3.  SRP single responsibility ptinciple*//
+ausgabeNamenSRP("jana","janzen");
+
+function ausgabeNamenSRP(firstName, familyName) {
+
+    // 1. Funktionalität: string composit
+    const GAP = " ";
+    let outputStr = "Hallo, " + firstName + " " + familyName + "!"
+   
+    console.log(outputStr);
+}
+
+
+
+
 
     
