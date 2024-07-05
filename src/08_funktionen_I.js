9d203e35923b81ca4600f36a0365cf0ff1eb305b
Ja0n0aJ <jana.janzen1@web.de>
2024-07-05 08:06:26 +0000

    09/ Rechner bauen

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -41,23 +41,38 @@
 // const prompt = require('prompt-sync')({sigint: true});
 // ausgabeNamenParams(prompt("Vorname?:"),prompt("Name?:"));
 
-function ausgabeNamenParams(firstName, familyName) {
-         console.log("Hallo, " + firstName + " " + familyName + "!");
-}
+//function ausgabeNamenParams(firstName, familyName) {
+         //console.log("Hallo, " + firstName + " " + familyName + "!");
+//}
 
 //*****Funktion 3.  SRP single responsibility ptinciple*//
-ausgabeNamenSRP("jana","janzen");
+//ausgabeNamenSRP("jana","janzen");
 
-function ausgabeNamenSRP(firstName, familyName) {
+// function ausgabeNamenSRP(firstName, familyName) {
 
-    // 1. Funktionalität: string composit
-    const GAP = " ";
-    let outputStr = "Hallo, " + firstName + " " + familyName + "!"
+//     // 1. Funktionalität: string composit
+//     const GAP = " ";
+//     let outputStr = "Hallo, " + firstName + " " + familyName + "!"
    
-    console.log(outputStr);
+//     console.log(outputStr);
+// }
+
+/******Funktionen 3b.******/
+//1. Funktionalität: string composing
+output(getString("Max", "Mütze"));
+function getString(firstName, familyName); {
+    const GAP = " ";
+    let outputStr = " Hallo, " + firstName + GAP + familyName + "!";
+    return outputStr;    
 }
 
-
+// //2. Funktionalität string output
+// output("Hi");
+// output(2);
+// output(true);
+ function output(outputData) {
+     console.log(outputData);
+ }
 
 
 
