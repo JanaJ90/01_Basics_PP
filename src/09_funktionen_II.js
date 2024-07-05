1fc45fcbcbb5d24a5394d8339b6a6e3f2226dbec
Ja0n0aJ <jana.janzen1@web.de>
2024-07-05 10:27:03 +0000

    09/ 06 startApp/ get Num/get Op

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -10,16 +10,74 @@
 4. Ausgabe in Konsole : 
 */
 
-const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
 
+const prompt = require('prompt-sync')({sigint: true});
+
+
+const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
+const ERROR_STR_GEN = "Irgendwas ging schief";
+
+startApp()
+function startApp() {
+	output(calculator(getNum1(),getOp(),getNum2()));
+}
+
+function getNum1() {
+	return parseInt(prompt("Zahl1?:"));
+}
+function getOp() {
+		return prompt ("OP?:");
+	}
+
+function getNum2() {
+	return parseInt(prompt("Zahl2?:"));
+}
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
+
+function calculator(a,b,op) {   //* WAS soll gemacht werden? *//
+	switch (op) {
+		case "+":
+			return  add(a,b);
+		
+
+		case "-":	
+			return subtract(a,b);
+		
+
+		case "*":
+			return multiply(a,b);
+		
+
+		case "/":
+		case ":":
+			return divide(a,b);
+			
+		
+		
+		default:
+			return ERROR_STR_GEN;
+			
+	}
+}
+
+								//* WIE soll es gemacht werden? *//
 
 // module: division a / b |  test:
-output(divide(4,2));
-output(divide(3,2));
-output(divide(3,-2));
-output(divide(0,2));
-output(divide(3,0));
-output(divide(0,0));
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(0,2));
+// output(divide(3,0));
+// output(divide(0,0));
 
 function divide(a,b) {
 
@@ -43,10 +101,6 @@ function multiply(a,b) {
 	return  a * b;
 }
 
-
-
-
-
 // module: subtraction a - b |  test:
 // output(subtract(3,2));
 // output(subtract(3,-2));
@@ -55,12 +109,6 @@ function multiply(a,b) {
 function subtract(a,b) {
 	return a - b;
 }
-
-
-
-
-
-
 
 // module: addition a + b |  test:
 
@@ -71,8 +119,6 @@ function subtract(a,b) {
 function add(a,b) {
 	return a + b;
 }
-
-
 
 // module: output | test:
 // output("hello");
